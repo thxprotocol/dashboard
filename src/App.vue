a6c65259f18c381d12d4e839cb039e6794b88ce2
Peter Polman <peter@peterpolman.nl>
2021-05-04 12:18:45 +0200

    Adds GTM to views

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,28 @@
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-// import { mapGetters } from 'vuex';
 import BaseNavbar from './components/BaseNavbar.vue';
 
 @Component({
-    components: { 'base-navbar': BaseNavbar },
-    // computed: mapGetters({
-    //     user: 'account/user',
-    // }),
+    components: {
+        'base-navbar': BaseNavbar,
+    },
 })
-export default class Redirect extends Vue {}
+export default class App extends Vue {
+    created() {
+        (function (w: any, d, s, l: any, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                'event': 'gtm.js',
+            });
+            const f: any = d.getElementsByTagName(s)[0],
+                j: any = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', process.env.VUE_APP_GTM);
+    }
+}
 </script>
