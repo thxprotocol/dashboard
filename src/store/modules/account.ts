0cf58f434149405ef0b719d5ab48081afbf716e3
Peter Polman <peter@peterpolman.nl>
2021-10-26 16:32:15 +0200

    Correct env...

--- src/store/modules/account.ts
+++ src/store/modules/account.ts
@@ -30,7 +30,7 @@ export interface UserProfile {
 }
 
 const config: any = {
-    authority: process.env.VUE_APP_AUTH_ROOT,
+    authority: process.env.VUE_APP_AUTH_URL,
     client_id: process.env.VUE_APP_OIDC_CLIENT_ID,
     client_secret: process.env.VUE_APP_OIDC_CLIENT_SECRET,
     redirect_uri: `${BASE_URL}/signin-oidc`,
@@ -88,7 +88,7 @@ class AccountModule extends VuexModule {
         try {
             const r = await axios({
                 method: 'GET',
-                url: process.env.VUE_APP_AUTH_ROOT + '/me',
+                url: process.env.VUE_APP_AUTH_URL + '/me',
             });
 
             if (r.status !== 200) {
