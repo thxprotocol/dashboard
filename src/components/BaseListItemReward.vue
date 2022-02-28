c51ed72908ce64a17a61c5b442194e91d2ab4975
Peter Polman <peter@peterpolman.nl>
2022-02-28 09:56:11 +0100

    Adds reward state toggle

--- src/components/BaseListItemReward.vue
+++ src/components/BaseListItemReward.vue
@@ -2,25 +2,19 @@
     <b-form-group class="mb-0">
         <hr />
         <div class="row pt-2 pb-2">
-            <div class="col-md-1 d-flex align-items-center">
-                <span class="large mr-2 text-primary">#{{ reward.id }}</span>
+            <div class="col-md-1 d-flex align-items-center justify-content-center">
+                <i
+                    v-b-tooltip
+                    :title="`ID #${reward.id}`"
+                    class="fas fa-power-off ml-0 mr-2"
+                    :class="{ 'text-danger': !reward.state, 'text-success': reward.state }"
+                ></i>
+                <span class="large mr-2 text-primary"></span>
                 <template v-if="reward.poll">
                     <a :id="`rewardPoll-${reward.id}`">
                         <i class="fas fa-poll text-primary"></i>
                     </a>
-                    <b-popover :target="`rewardPoll-${reward.id}`" triggers="hover" placement="top">
-                        <template #title>Active poll</template>
-
-                        <p>
-                            Start {{ reward.poll.startTime }}<br />
-                            End:{{ reward.poll.endTime }}
-                        </p>
-
-                        <p>
-                            Yes: {{ reward.poll.yesCounter }}<br />
-                            No: {{ reward.poll.noCounter }}
-                        </p>
-                    </b-popover>
+                    <base-popover-reward-poll :target="`rewardPoll-${reward.id}`" :reward="reward" />
                 </template>
             </div>
             <div class="col-md-3">
@@ -33,7 +27,7 @@
                     <b-form-input type="number" v-model="reward.withdrawDuration" />
                 </b-input-group>
             </div>
-            <div :class="isGovernanceEnabled ? 'col-md-3' : 'col-md-6 '" class="d-flex align-items-center">
+            <div class="col-md-3">
                 <a v-if="channelItemURL" target="_blank" :href="channelItemURL">
                     <b-badge class="border p-2" variant="light">
                         <img
@@ -47,18 +41,24 @@
                     </b-badge>
                 </a>
             </div>
-            <div class="col-md-2 d-flex justify-content-end">
-                <b-button class="rounded-pill mr-2" variant="light" v-b-modal="`modal-reward-link-${reward.id}`">
-                    <i class="fas fa-link ml-0 text-primary"></i>
-                </b-button>
-                <base-modal-reward-link :assetPool="assetPool" :reward="reward" :rewardURL="rewardURL" />
-                <b-button class="rounded-pill mr-2" variant="light" v-b-modal="`modal-reward-qrcode-${reward.id}`">
-                    <i class="fas fa-qrcode ml-0 text-primary"></i>
-                </b-button>
-                <base-modal-reward-qrcode :assetPool="assetPool" :reward="reward" :rewardURL="rewardURL" />
-                <!-- <b-button class="rounded-pill cursor-not-allowed" disabled variant="light" @click="updateReward()">
-                    <i class="fas fa-save ml-0 text-primary"></i>
-                </b-button> -->
+            <div class="col-md-5">
+                <div class="d-flex justify-content-end">
+                    <b-button class="rounded-pill mr-2" variant="light" v-b-modal="`modal-reward-link-${reward.id}`">
+                        <i class="fas fa-link ml-0 text-primary"></i>
+                    </b-button>
+                    <base-modal-reward-link :assetPool="assetPool" :reward="reward" :rewardURL="rewardURL" />
+                    <b-button class="rounded-pill mr-2" variant="light" v-b-modal="`modal-reward-qrcode-${reward.id}`">
+                        <i class="fas fa-qrcode ml-0 text-primary"></i>
+                    </b-button>
+                    <base-modal-reward-qrcode :assetPool="assetPool" :reward="reward" :rewardURL="rewardURL" />
+                    <b-button class="rounded-pill" variant="light" @click="toggleState()">
+                        <i
+                            class="fas fa-power-off ml-0 mr-2"
+                            :class="{ 'text-danger': reward.state, 'text-success': !reward.state }"
+                        ></i>
+                        {{ reward.state ? 'Disable' : 'Enable' }}
+                    </b-button>
+                </div>
             </div>
         </div>
     </b-form-group>
@@ -66,22 +66,17 @@
 
 <script lang="ts">
 import { AssetPool } from '@/store/modules/assetPools';
-import { Reward, ChannelType, ChannelAction } from '@/store/modules/rewards';
-import { BBadge, BButton, BFormGroup, BFormInput, BInputGroup, BPopover } from 'bootstrap-vue';
+import { Reward, ChannelType, ChannelAction, RewardState } from '@/store/modules/rewards';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import BaseModalRewardLink from '@/components/BaseModalRewardLink.vue';
-import BaseModalRewardQRCode from '@/components/BaseModalRewardQRCode.vue';
+import BaseModalRewardQrcode from '@/components/BaseModalRewardQRCode.vue';
+import BasePopoverRewardPoll from '@/components/BasePopoverRewardPoll.vue';
 
 @Component({
     components: {
-        'base-modal-reward-link': BaseModalRewardLink,
-        'base-modal-reward-qrcode': BaseModalRewardQRCode,
-        'b-button': BButton,
-        'b-badge': BBadge,
-        'b-form-group': BFormGroup,
-        'b-input-group': BInputGroup,
-        'b-popover': BPopover,
-        'b-form-input': BFormInput,
+        BaseModalRewardLink,
+        BaseModalRewardQrcode,
+        BasePopoverRewardPoll,
     },
 })
 export default class BaseListItemReward extends Vue {
@@ -133,7 +128,14 @@ export default class BaseListItemReward extends Vue {
         };
         return `${this.walletUrl}/claim?hash=${btoa(JSON.stringify(data))}`;
     }
+
+    toggleState() {
+        this.$store.dispatch('rewards/update', {
+            reward: this.reward,
+            data: {
+                state: this.reward.state ? RewardState.Disabled : RewardState.Enabled,
+            },
+        });
+    }
 }
 </script>
-
-<style></style>
