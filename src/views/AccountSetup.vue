0ef73c336d3a0e204c62683f8230b88609bd8276
GarfDev <garfdev.13@gmail.com>
2022-03-14 19:58:14 +0700

    feat: integrate with mfa

---
+++ src/views/AccountSetup.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+    components: {},
+})
+export default class AccountSetup extends Vue {
+    async mounted() {
+        try {
+            await this.$store.dispatch('account/accountRedirect', {});
+        } catch (e) {
+            return;
+        }
+    }
+}
+</script>
