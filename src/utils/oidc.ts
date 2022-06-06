5af82807362fc9483c2b969beef2efcfa13dce19
Peter Polman <peter@peterpolman.nl>
2022-06-06 14:32:04 +0200

    Fix dashboard client scope

--- src/utils/oidc.ts
+++ src/utils/oidc.ts
@@ -1,6 +1,6 @@
 import { BASE_URL } from './secrets';
-export const ADMIN_SCOPE = 'openid account:read account:write asset_pools:read asset_pools:write rewards:read members:read members:write withdrawals:write';
-export const DASHBOARD_SCOPE = 'openid asset_pools:read asset_pools:write withdrawals:read rewards:write deposits:read deposits:write promotions:read promotions:write';
+export const ADMIN_SCOPE =
+    'openid account:read account:write rewards:read members:read members:write withdrawals:write';
 export const config: any = {
     authority: process.env.VUE_APP_AUTH_URL,
     client_id: process.env.VUE_APP_OIDC_CLIENT_ID,
@@ -12,5 +12,6 @@ export const config: any = {
     silent_redirect_uri: `${BASE_URL}/silent-renew`,
     automaticSilentRenew: true,
     loadUserInfo: true,
-    scope: DASHBOARD_SCOPE
+    scope:
+        'openid account:read pools:read pools:write erc20:read erc20:write erc721:read erc721:write rewards:read rewards:write withdrawals:read deposits:read deposits:write promotions:read promotions:write',
 };
