92d458cf03f3ff3ee638f7f83e961ca43770eb14
valeriagrazzini <vgrazzini@gmail.com>
2022-08-01 17:47:41 +0200

    added loading state to metadata overview

--- src/views/pool/Metadata.vue
+++ src/views/pool/Metadata.vue
@@ -1,47 +1,63 @@
 <template>
-    <div>
-        <b-row class="mb-3">
-            <b-col class="d-flex align-items-center">
-                <h2 class="mb-0">Metadata</h2>
-            </b-col>
-            <div class="d-flex ustify-content-end">
-                <b-col>
-                    <b-button v-b-modal="'modalNFTCreate'" class="rounded-pill" variant="primary">
-                        <i class="fas fa-plus mr-2"></i>
-                        <span class="d-none d-md-inline">Create Metadata</span>
-                    </b-button>
+    <b-skeleton-wrapper :loading="isLoading">
+        <template #loading>
+            <b-card class="mt-3 mb-3 shadow-sm cursor-pointer">
+                <b-skeleton animation="fade" width="65%"></b-skeleton>
+                <hr />
+                <b-skeleton animation="fade" width="55%"></b-skeleton>
+                <b-skeleton animation="fade" class="mb-3" width="70%"></b-skeleton>
+                <b-skeleton type="button" animation="fade" class="rounded-pill" width="100%"></b-skeleton>
+            </b-card>
+        </template>
+        <div>
+            <b-row class="mb-3">
+                <b-col class="d-flex align-items-center">
+                    <h2 class="mb-0">Metadata</h2>
                 </b-col>
-                <b-col>
-                    <b-button v-b-modal="'modalNFTBulkCreate'" class="rounded-pill" variant="primary">
-                        <i class="fas fa-upload mr-2"></i>
-                        <span class="d-none d-md-inline">Upload images</span>
-                    </b-button>
-                </b-col>
-                <b-col>
-                    <b-button v-b-modal="'modalNFTUploadMetadataCsv'" class="rounded-pill" variant="primary">
-                        <i class="fas fa-upload mr-2"></i>
-                        <span class="d-none d-md-inline">Upload spreadsheet</span>
-                    </b-button>
-                </b-col>
-            </div>
-        </b-row>
-        <base-nothing-here
-            v-if="erc721 && !erc721.metadata"
-            text-submit="Create NFT Metadata"
-            title="You have not created NFT Metadata yet"
-            description="NFT Metadata is the actual data that is attached to your token."
-            @clicked="$bvModal.show('modalNFTCreate')"
-        />
-        <base-card-erc721-metadata
-            v-if="erc721 && erc721.metadata"
-            :erc721="erc721"
-            :metadata="erc721.metadata"
-            :pool="pool"
-        />
-        <base-modal-erc721-metadata-create v-if="erc721" :pool="pool" :erc721="erc721" />
-        <base-modal-erc721-metadata-bulk-create v-if="erc721" :pool="pool" :erc721="erc721" @success="listMetadata()" />
-        <BaseModalErc721MetadataUploadCSV v-if="erc721" :pool="pool" :erc721="erc721" @success="listMetadata()" />
-    </div>
+                <div class="d-flex ustify-content-end">
+                    <b-col>
+                        <b-button v-b-modal="'modalNFTCreate'" class="rounded-pill" variant="primary">
+                            <i class="fas fa-plus mr-2"></i>
+                            <span class="d-none d-md-inline">Create Metadata</span>
+                        </b-button>
+                    </b-col>
+                    <b-col>
+                        <b-button v-b-modal="'modalNFTBulkCreate'" class="rounded-pill" variant="primary">
+                            <i class="fas fa-upload mr-2"></i>
+                            <span class="d-none d-md-inline">Upload images</span>
+                        </b-button>
+                    </b-col>
+                    <b-col>
+                        <b-button v-b-modal="'modalNFTUploadMetadataCsv'" class="rounded-pill" variant="primary">
+                            <i class="fas fa-upload mr-2"></i>
+                            <span class="d-none d-md-inline">Upload spreadsheet</span>
+                        </b-button>
+                    </b-col>
+                </div>
+            </b-row>
+            <base-nothing-here
+                v-if="erc721 && !erc721.metadata"
+                text-submit="Create NFT Metadata"
+                title="You have not created NFT Metadata yet"
+                description="NFT Metadata is the actual data that is attached to your token."
+                @clicked="$bvModal.show('modalNFTCreate')"
+            />
+            <base-card-erc721-metadata
+                v-if="erc721 && erc721.metadata"
+                :erc721="erc721"
+                :metadata="erc721.metadata"
+                :pool="pool"
+            />
+            <base-modal-erc721-metadata-create v-if="erc721" :pool="pool" :erc721="erc721" />
+            <base-modal-erc721-metadata-bulk-create
+                v-if="erc721"
+                :pool="pool"
+                :erc721="erc721"
+                @success="listMetadata()"
+            />
+            <BaseModalErc721MetadataUploadCSV v-if="erc721" :pool="pool" :erc721="erc721" @success="listMetadata()" />
+        </div>
+    </b-skeleton-wrapper>
 </template>
 
 <script lang="ts">
@@ -78,6 +94,7 @@ export default class MetadataView extends Vue {
 
     pools!: IPools;
     erc721s!: IERC721s;
+    isLoading = false;
 
     get pool(): IPool {
         return this.pools[this.$route.params.id];
@@ -87,10 +104,12 @@ export default class MetadataView extends Vue {
         return this.erc721s[this.pool.token._id];
     }
 
-    listMetadata() {
-        this.$store.dispatch('erc721/read', this.pool.token._id).then(async () => {
+    async listMetadata() {
+        this.isLoading = true;
+        await this.$store.dispatch('erc721/read', this.pool.token._id).then(async () => {
             await this.$store.dispatch('erc721/listMetadata', this.erc721);
         });
+        this.isLoading = false;
     }
     mounted() {
         this.listMetadata();
