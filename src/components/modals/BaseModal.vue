e4e885667e4ce7729d4fb6b6d5f231a6e947d3e2
Peter <peter@thx.network>
2022-08-18 11:23:46 +0200

    Fix loading in modal

--- src/components/modals/BaseModal.vue
+++ src/components/modals/BaseModal.vue
@@ -11,9 +11,12 @@
         :hide-header="loading"
     >
         <div class="w-100 center-center bg-light py-3" v-if="loading">
-            <div class="text-center">
-                <b-spinner variant="gray" /><br />
-                <strong class="text-gray">Working on it...</strong>
+            <div class="text-center w-100">
+                <!-- <b-spinner variant="gray" /><br /> -->
+                <i class="fas fa-hourglass-half text-gray mb-2" style="font-size: 2rem"> </i>
+                <p class="text-muted">Please give us ~20 seconds to process your request.</p>
+                <b-progress class="w-100 my-3" variant="gray" :value="100" animated></b-progress>
+                <p class="text-muted small">{{ info }}</p>
             </div>
         </div>
         <b-alert variant="danger" show v-if="error">
@@ -37,5 +40,28 @@ export default class BaseModal extends Vue {
     @Prop() loading!: boolean;
     @Prop() error!: string;
     @Prop() hideFooter!: boolean;
+    interval: any = null;
+    info = '';
+
+    mounted() {
+        const messages = [
+            'Preparing network request...',
+            'Sending network request...',
+            'Interacting with the network...',
+            'Reading data from the network...',
+            'Preparing network response...',
+        ];
+        let index = 0;
+        this.info = messages[index];
+        this.interval = setInterval(() => {
+            if (index >= messages.length) index = 0;
+            this.info = messages[index++];
+        }, 3000);
+    }
+
+    beforeDestroy() {
+        clearInterval(this.interval);
+        this.interval = null;
+    }
 }
 </script>
